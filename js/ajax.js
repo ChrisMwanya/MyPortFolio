043b511e9402d5412c64598de5f35bb13b048ef2
ChrisMwanya <cmwanya@gmail.com>
2021-04-08 17:42:02 +0100

    Amelioration json

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,32 @@
 
 
 window.onload = () => {
+
+    // my identity
     const names = document.getElementById('names');
     const fonction = document.getElementById('fonction')
-    fetch("http://localhost:3000/identity").then(function (response) {
+
+    fetch("https://my-json-server.typicode.com/ChrisMwanya/MyPortFolio/identity").then(function (response) {
         
         return response.json();
     }).then(function (data){        
         for (const iterator of data) {            
             names.textContent = iterator.nom + " " + iterator.prenom
             fonction.textContent = iterator.fonction;
+        }
+        
+    });
+
+    //about Me
+
+    const greating = document.getElementById('greating');
+
+    fetch("https://my-json-server.typicode.com/ChrisMwanya/MyPortFolio/about").then(function (response) {
+        
+        return response.json();
+    }).then(function (data){        
+        for (const iterator of data) {            
+           greating.textContent = iterator.biography
         }
         
     });
